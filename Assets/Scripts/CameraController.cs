e475f05d45ebe754a7d89e77d464b7512efc0024
Kinhs <bangtanboy7b@gmail.com>
2025-10-06 21:28:28 +0700

    - Keep the camera confined

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
+    private Camera cam;
+    private float halfWidth, halfHeight;
     private Transform target;
+
+    public Transform clampMin, clampMax;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = FindAnyObjectByType<PlayerController>().transform; // <PlayerController>
+        clampMin.SetParent(null);
+        clampMax.SetParent(null);
+
+        cam = GetComponent<Camera>();
+        halfHeight = cam.orthographicSize;
+        halfWidth = cam.orthographicSize * cam.aspect;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        Vector3 clampedPosition = transform.position;
+
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, clampMin.position.x + halfWidth, clampMax.position.x - halfWidth);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, clampMin.position.y + halfHeight, clampMax.position.y - halfHeight);
+
+        transform.position = clampedPosition;
     }
 }
