42e39dd2e79a0326b1458f65039e8c5f50274a93
VietHa1712 <nguyenvietha.17122003@gmail.com>
2025-11-25 18:41:01 +0700

    + Add some NPCs

---
+++ Assets/Scripts/NPC/NpcCallout.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class NpcCallout : MonoBehaviour
+{
+    public GameObject[] calloutObjects;
+    public float minInterval = 4f;
+    public float maxInterval = 7f;
+    public float displayDuration = 3f;
+
+    private float timer;
+    private float nextTime;
+    private float displayTimer;
+    private int currentIndex = -1;
+    private int lastIndex = -1;
+
+    void Start()
+    {
+        for (int i = 0; i < calloutObjects.Length; i++)
+            calloutObjects[i].SetActive(false);
+
+        nextTime = Random.Range(minInterval, maxInterval);
+    }
+
+    void Update()
+    {
+        if (currentIndex == -1)
+        {
+            timer += Time.deltaTime;
+            if (timer >= nextTime)
+            {
+                int index;
+                do { index = Random.Range(0, calloutObjects.Length); }
+                while (index == lastIndex && calloutObjects.Length > 1);
+
+                lastIndex = index;
+                currentIndex = index;
+                calloutObjects[currentIndex].SetActive(true);
+
+                displayTimer = 0f;
+                timer = 0f;
+            }
+        }
+        else
+        {
+            displayTimer += Time.deltaTime;
+            if (displayTimer >= displayDuration)
+            {
+                calloutObjects[currentIndex].SetActive(false);
+                currentIndex = -1;
+                nextTime = Random.Range(minInterval, maxInterval);
+            }
+        }
+    }
+}
